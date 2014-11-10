53a02aaadaa0ea056970dd4a966e6fc55751d03f
yoshuawuyts <i@yoshuawuyts.com>
2014-11-10 14:27:25 +0100

    routes: init

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@
  */
 
 var responseTime = require('koa-response-time');
+var trieRouter = require('koa-trie-router');
 var compress = require('koa-compress');
 var compose = require('koa-compose');
 var session = require('koa-session');
@@ -10,6 +11,9 @@ var logger = require('koa-logger');
 var helmet = require('koa-helmet');
 var parse = require('co-body');
 var koa = require('koa');
+
+var users = require('./users/index');
+var home = require('./home/index');
 
 var env = process.env.NODE_ENV || 'development';
 var port = process.env.port || 1337;
@@ -34,12 +38,24 @@ if ('test' != process.env.NODE_ENV) app.use(logger());
 app.use(responseTime());
 
 /**
+ * Routes.
+ */
+
+app.use(trieRouter(app));
+home(app);
+users(app);
+app.use(function* routes(next) {
+
+});
+
+/**
  * Start listening.
  */
 
 if (!module.parent) {
   console.log('');
-  console.log('Port: ' + port);
-  console.log('Env:  ' + env);
+  console.log('  Port: ' + port);
+  console.log('  Env:  ' + env);
+  console.log('');
   app.listen(port);
 }
---
+++ api/users/index.js
@@ -0,0 +1,28 @@
+/**
+ * Expose `routes()`.
+ */
+
+module.exports = routes;
+
+/**
+ * Routes().
+ *
+ * @param {Function} router
+ * @api public
+ */
+
+function routes(router) {
+  var users = router.route('/users');
+
+  users.get(function* (next) {
+    this.body = 'hello world';
+  });
+
+  users.post(function* (next) {
+    // do stuff
+  });
+
+  users.patch(function* (next) {
+    // do stuff
+  });
+}
